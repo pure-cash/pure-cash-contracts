2e80093c186f12c79edd844743979d62142483ea
paulxYx <paul.xYx@proton.me>
2024-08-26 15:34:38 +0800

    remove unused error

--- contracts/core/interfaces/IPUSD.sol
+++ contracts/core/interfaces/IPUSD.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IPUSD is IERC20 {
-    error InvalidMinter();
-
     function mint(address to, uint256 value) external;
 
     function burn(uint256 value) external;
